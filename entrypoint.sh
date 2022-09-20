8650183cc67a8b4971c16d4d3747a4db07448b03
Thomas Nairn <thomas@thenairn.com>
2022-09-20 19:53:19 +0000

    Try regular mysql driver

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,6 @@ envsubst < /opt/traccar/conf/traccar.env.xml > /opt/traccar/conf/traccar.xml
 
 echo "/||||||||||||||||||||||\ STARTING /|||||||||||||||||||||||||\\"
 echo ""
-echo "Listing cloud sql instances..."
-echo ""
-ls /cloudsql/
-echo ""
 echo "Config is as follows"
 echo ""
 cat /opt/traccar/conf/traccar.xml

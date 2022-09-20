1fb91e06e89153b02140585869b120cd544e7ed9
Thomas Nairn <thomas@thenairn.com>
2022-09-20 19:46:33 +0000

    Print some debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,16 @@
 
 envsubst < /opt/traccar/conf/traccar.env.xml > /opt/traccar/conf/traccar.xml
 
+echo "/||||||||||||||||||||||\ STARTING /|||||||||||||||||||||||||\\"
+echo ""
+echo "Listing cloud sql instances..."
+echo ""
+ls /cloudsql/
+echo ""
+echo "Config is as follows"
+echo ""
+cat /opt/traccar/conf/traccar.xml
+echo ""
+echo ""
+
 exec "$@"

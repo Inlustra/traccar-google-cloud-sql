496f90e5001a5f9435d50623f7af3455c916bc1f
Thomas Nairn <thomas@thenairn.com>
2022-09-21 12:41:10 +0000

    Moved driver download to runtime to allow loading a custom binary

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,11 @@
 
 FROM traccar/traccar:5-alpine
 ENV DEFAULT_FILE_LOCATION /opt/traccar/conf/default.xml
-RUN apk add gettext
+RUN apk --no-cache add curl gettext
 
 ENV LOG_LEVEL info
 ENV DATABASE_TYPE mysql
 ENV JDBC_URL_OPTIONS ""
-
-COPY ./mysql-socket-factory-1.7.0-jar-with-driver-and-dependencies.jar /mysql-socket-factory.jar
-COPY ./postgres-socket-factory-1.7.0-jar-with-driver-and-dependencies.jar /postgres-socket-factory.jar
 
 COPY entrypoint.sh /entrypoint.sh
 COPY ./traccar.env.xml /opt/traccar/conf/traccar.env.xml
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,30 +1,35 @@
 #!/bin/sh
 
-if [ -n "$DATABASE_TYPE" ]; then
-    echo "Database type set to $DATABASE_TYPE"
-    if [ "$DATABASE_TYPE" == "mysql" ]; then
-        echo "Detected mysql, setting variables"
-        export DATABASE_PROTOCOL=mysql
-        export DATABASE_DRIVER="com.mysql.cj.jdbc.Driver"
-        export DATABASE_SOCKET_FACTORY="com.google.cloud.sql.mysql.SocketFactory"
-        export DATABASE_DRIVER_FILE_LOCATION=/mysql-socket-factory.jar
-    elif [ "$DATABASE_TYPE" == "postgresql" ]; then
-        echo "Detected postgresql, setting variables"
-        export DATABASE_PROTOCOL=postgresql
-        export DATABASE_DRIVER="org.postgresql.Driver"
-        export DATABASE_SOCKET_FACTORY="com.google.cloud.sql.postgres.SocketFactory"
-        export DATABASE_DRIVER_FILE_LOCATION=/postgres-socket-factory.jar
-        if [ -n "$ENABLE_IAM_AUTH" ]; then
-            export JDBC_URL_OPTIONS="&amp;enableIamAuth=true"
-        fi
+echo "Database type set to $DATABASE_TYPE"
+if [ "$DATABASE_TYPE" == "mysql" ]; then
+    echo "Detected mysql, setting variables"
+    export DATABASE_PROTOCOL=mysql
+    export DATABASE_DRIVER="com.mysql.cj.jdbc.Driver"
+    export DATABASE_SOCKET_FACTORY="com.google.cloud.sql.mysql.SocketFactory"
+    export DATABASE_DRIVER_FILE_LOCATION=/mysql-socket-factory.jar
+    echo "Downloading driver file..."
+    curl -L https://storage.googleapis.com/cloud-sql-java-connector/v1.7.0/mysql-socket-factory-1.7.0-jar-with-driver-and-dependencies.jar > /mysql-socket-factory.jar
+elif [ "$DATABASE_TYPE" == "postgresql" ]; then
+    echo "Detected postgresql, setting variables"
+    export DATABASE_PROTOCOL=postgresql
+    export DATABASE_DRIVER="org.postgresql.Driver"
+    export DATABASE_SOCKET_FACTORY="com.google.cloud.sql.postgres.SocketFactory"
+    export DATABASE_DRIVER_FILE_LOCATION=/postgresql-socket-factory.jar
+    echo "Downloading driver file..."
+    curl -L https://storage.googleapis.com/cloud-sql-java-connector/v1.7.0/mysql-socket-factory-1.7.0-jar-with-driver-and-dependencies.jar > /postgresql-socket-factory.jar
+    if [ -n "$ENABLE_IAM_AUTH" ]; then
+        export JDBC_URL_OPTIONS="&amp;enableIamAuth=true"
     fi
 else
-    echo "Database type is unset, assuming advanced variables already set."
+    echo "Database type is unset, assuming advanced variables already set"
+    echo "Downloading driver file from $DRIVER_FILE_URL to /socket-factory.jar"
+    curl -L $DRIVER_FILE_URL > /socket-factory.jar
+    export DATABASE_DRIVER_FILE_LOCATION=/socket-factory.jar
 fi
 
 envsubst < /opt/traccar/conf/traccar.env.xml > /opt/traccar/conf/traccar.xml
 
-echo "/||||||||||||||||||||||\ STARTING /|||||||||||||||||||||||||\\"
+echo "/||||||||||||||||||||||\ STARTING /||||||||||||||||||||||\\"
 echo ""
 echo "Config is as follows"
 echo ""
